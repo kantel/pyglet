481da3a684247531a92b30f09c30391a140a9f7e
Jörg Kantel <joerg@kantel.de>
2018-08-16 18:25:36 +0200

    Space Invaders Stage 2

--- sources/invaders/gameobjects.py
+++ sources/invaders/gameobjects.py
@@ -1,15 +1,34 @@
 import pyglet
+from pyglet.sprite import Sprite
+
+def preload_image(image):
+    img = pyglet.image.load("images/" + image)
+    return img
+
+def preload_image_animation(image, image_row, image_col, image_width, image_height):
+    img = pyglet.image.load("images/" + image)
+    img_seq = pyglet.image.ImageGrid(img, image_row, image_col, item_width = image_width, item_height = image_height)
+    img_texture = pyglet.image.TextureGrid(img_seq)
+    img_anim = pyglet.image.Animation.from_image_sequence(img_texture[:], 0.1, loop = True)
+    return img_anim
 
 class GameObject():
     
-    def __init__(self, posx, posy, image = None):
+    def __init__(self, posx, posy, sprite = None):
         self.posx = posx
         self.posy = posy
         self.velx = 0
         self.vely = 0
-        if image is not None:
-            image = pyglet.image.load("images/" + image)
-            image_seq = pyglet.image.ImageGrid(image, 4, 1, item_width = 64, item_height = 29)
-            image_texture = pyglet.image.TextureGrid(image_seq)
-            image_anim = pyglet.image.Animation.from_image_sequence(image_texture[:], 0.1, loop = True)
-            self.sprite = pyglet.sprite.Sprite(image_anim, self.posx, self.posy)
+        if sprite is not None:
+            self.sprite = sprite
+            self.sprite.x = self.posx
+            self.sprite.y = self.posy
+    
+    def draw(self):
+        self.sprite.draw()
+    
+    def update(self, dt):
+        self.sprite.x += self.velx*dt
+        self.sprite.y += self.vely*dt
+        self.posx = self.sprite.x
+        self.posy = self.sprite.y
---
+++ sources/invaders/stage02.py
@@ -0,0 +1,69 @@
+import pyglet
+from pyglet.window import FPSDisplay, key
+from pyglet.sprite import Sprite
+from gameobjects import GameObject, preload_image, preload_image_animation
+import os
+
+TITLE = "Space Invaders Stage 2"
+PLAYERSPEED = 300
+SPACESPEED = -50
+
+class GameWindow(pyglet.window.Window):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_location(100, 100)
+        self.frame_rate = 1/60.0
+        self.fps_display = FPSDisplay(self)
+        self.fps_display.label.font_size = 24
+        
+        # Hier wird der Pfad zum Verzeichnis des ».py«-Files gesetzt
+        # Erspart einem das Herumgehample in TextMate mit dem os.getcwd()
+        # und os.path.join()
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+        
+        self.space_list = []
+        self.space_img = preload_image("farback.gif")
+        for i in range(2):
+            self.space_list.append(GameObject(i*1782, 0, Sprite(self.space_img)))
+        
+        for space in self.space_list:
+            space.velx = SPACESPEED
+        
+        player_spr = Sprite(preload_image_animation("Spritesheet_64x29.png", 4, 1, 64, 29))
+        self.player = GameObject(50, 300, player_spr)
+    
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.UP:
+            self.player.vely = PLAYERSPEED
+        if symbol == key.DOWN:
+            self.player.vely = -PLAYERSPEED
+    
+    def on_key_release(self, symbol, modifiers):
+        if symbol in (key.UP, key.DOWN):
+            self.player.vely = 0
+    
+    def on_draw(self):
+        self.clear()
+        for space in self.space_list:
+            space.draw()
+        self.player.draw()
+        
+        self.fps_display.draw()
+    
+    def update_space(self, dt):
+        for space in self.space_list:
+            space.update(dt)
+            if space.posx <= -1882:
+                self.space_list.remove(space)
+                self.space_list.append(GameObject(1682, 0, Sprite(self.space_img)))
+            space.velx = SPACESPEED
+    
+    def update(self, dt):
+        self.player.update(dt)
+        self.update_space(dt)
+
+win = GameWindow(900, 600, TITLE, resizable = False)
+pyglet.clock.schedule_interval(win.update, win.frame_rate)
+pyglet.app.run()
